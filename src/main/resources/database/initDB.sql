b9fb9e9dc35a02f3da0c564eb1b5bd92ab1e2e8a
Dmitrii Gurylev <gurylev.dmitrii@mail.ru>
2022-06-19 18:42:26 +0300

    init db commented

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -1,15 +1,15 @@
-create table if not exists shop_unit
-(
-    id                         varchar,
-    name                       varchar,
-    price                      double precision,
-    parent_id                  varchar,
-    type                       varchar,
-    date                       timestamp
-);
-
-create table if not exists relation
-(
-    parent_id varchar,
-    child_id  varchar
-);
+-- create table if not exists shop_unit
+-- (
+--     id                         varchar,
+--     name                       varchar,
+--     price                      double precision,
+--     parent_id                  varchar,
+--     type                       varchar,
+--     date                       timestamp
+-- );
+--
+-- create table if not exists relation
+-- (
+--     parent_id varchar,
+--     child_id  varchar
+-- );
