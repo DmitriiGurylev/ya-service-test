0c6e0b91fa48c7cd07c48b873075cf694f18f72e
Dmitrii Gurylev <gurylev.dmitrii@mail.ru>
2022-06-26 14:02:37 +0300

    added tests

---
+++ src/test/java/ya/project/TestImport.java
@@ -0,0 +1,85 @@
+package ya.project;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ya.project.Repository.ElementRepository;
+import ya.project.Repository.RelationRepository;
+import ya.project.Service.Service;
+import ya.project.Units.ShopUnitImportRequest;
+
+import java.io.File;
+import java.io.IOException;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestImport {
+
+    @Autowired
+    Service service;
+    @Autowired
+    ElementRepository elementRepository;
+    @Autowired
+    RelationRepository relationRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeAll
+    public void beforeAll() throws IOException {
+        ShopUnitImportRequest itemsToImport = objectMapper.readValue(
+                new File("src/test/resources/importValid1.json"), ShopUnitImportRequest.class
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    public void successImport1() throws IOException {
+        ShopUnitImportRequest itemsToImport = objectMapper.readValue(
+                new File("src/test/resources/importValid1.json"), ShopUnitImportRequest.class
+        );
+        boolean imported = service.importItems(itemsToImport);
+        Assertions.assertTrue(imported);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void successImport2() throws IOException {
+        ShopUnitImportRequest itemsToImport = objectMapper.readValue(
+                new File("src/test/resources/importValid2.json"), ShopUnitImportRequest.class
+        );
+        boolean imported = service.importItems(itemsToImport);
+        Assertions.assertTrue(imported);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void successImport3() throws IOException {
+        ShopUnitImportRequest itemsToImport = objectMapper.readValue(
+                new File("src/test/resources/importValid3.json"), ShopUnitImportRequest.class
+        );
+        boolean imported = service.importItems(itemsToImport);
+        Assertions.assertTrue(imported);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void successImport4() throws IOException {
+        ShopUnitImportRequest itemsToImport = objectMapper.readValue(
+                new File("src/test/resources/importValid4.json"), ShopUnitImportRequest.class
+        );
+        boolean imported = service.importItems(itemsToImport);
+        Assertions.assertTrue(imported);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void afterAll() {
+        elementRepository.deleteById("1");
+        elementRepository.deleteById("2");
+        elementRepository.deleteById("3");
+        elementRepository.deleteById("4");
+        elementRepository.deleteById("5");
+        elementRepository.deleteById("6");
+        elementRepository.deleteById("7");
+        elementRepository.deleteById("8");
+    }
+
+}
